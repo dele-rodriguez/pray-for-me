da9ece07ab0728bc803ca5ead131c4d76100f62f
dele-rodriguez <j.20rodriguezdele@gmail.com>
2024-03-16 19:16:18 +0100

    update 2.3

--- src/App.jsx
+++ src/App.jsx
@@ -28,36 +28,38 @@ function App() {
     }
   }, []);
 
-  useEffect(() => {
-    if (!ishidden) {
-      const timeoutId = setTimeout(async () => {
-        setIsHidden(true);
-        const newPrivateCount = privateCount + 1;
-        setPrivateCount(newPrivateCount);
-        try {
-          await updateDoc(collectionRef, {
-            Number: newPrivateCount
-          });
-          alert("Your Prayer has been Tracked Successfully");
-        } catch (error) {
-          console.error("Error updating Number: ", error);
-        }
-      }, 60000);
-      return () => clearTimeout(timeoutId); // Cleanup function to clear the timeout on component unmount or when ishidden changes
-    }
-  }, [ishidden]);
-  
-
-  // const setData = async () => {
-  //   try {
-  //     await setDoc(doc(db, "myCollection", "store"), {
-  //       number: 0
-  //     });
-  //     console.log("Document successfully written!");
-  //   } catch (e) {
-  //     console.error("Error writing document: ", e);
+  // useEffect(() => {
+  //   if (!ishidden) {
+  //     const timeoutId = setTimeout(async () => {
+  //       setIsHidden(true);
+  //       const newPrivateCount = privateCount + 1;
+  //       setPrivateCount(newPrivateCount);
+  //       try {
+  //         await updateDoc(collectionRef, {
+  //           Number: newPrivateCount
+  //         });
+  //         alert("Your Prayer has been Tracked Successfully");
+  //       } catch (error) {
+  //         console.error("Error updating Number: ", error);
+  //       }
+  //     }, 60000);
+  //     return () => clearTimeout(timeoutId); // Cleanup function to clear the timeout on component unmount or when ishidden changes
   //   }
-  // };
+  // }, [ishidden]);
+
+  const handlePrayerIncrement = async() => {
+    setIsHidden(true);
+    const newPrivateCount = privateCount + 1;
+    setPrivateCount(newPrivateCount);
+    try {
+      await updateDoc(collectionRef, {
+        Number: newPrivateCount
+      });
+      alert("Your Prayer has been Tracked Successfully");
+    } catch (error) {
+      console.error("Error updating Number: ", error);
+    }
+  }
 
   const handleIncrement = async () => {
     const newCount = count + 1;
@@ -89,12 +91,18 @@ function App() {
   return (
     <>
       <section className='bg flex flex-col justify-center items-center font-josefin relative text-black bg-scroll text-base'> 
-        <div className={`${ishidden ? "hidden" : "fixed"} bg-cream border-2 border-black  -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-[320px] p-2 px-3 rounded-xl h-[330px] flex flex-col justify-around items-center`}>
-          <p>
+        <div className={`${ishidden ? "hidden" : "fixed"} bg-cream border-2 border-black  -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-[320px] p-2 px-3 rounded-xl h-fit flex flex-col justify-around items-center`}>
+          <svg onClick={() => {handlePrayerIncrement()}} className='self-end absolute right-2 top-2 border border-black rounded' width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M4.29302 4.29289C4.68354 3.90237 5.3167 3.90237 5.70723 4.29289L10.0001 8.58579L14.293 4.29289C14.6835 3.90237 15.3167 3.90237 15.7072 4.29289C16.0978 4.68342 16.0978 5.31658 15.7072 5.70711L11.4143 10L15.7072 14.2929C16.0978 14.6834 16.0978 15.3166 15.7072 15.7071C15.3167 16.0976 14.6835 16.0976 14.293 15.7071L10.0001 11.4142L5.70723 15.7071C5.3167 16.0976 4.68354 16.0976 4.29302 15.7071C3.90249 15.3166 3.90249 14.6834 4.29302 14.2929L8.58591 10L4.29302 5.70711C3.90249 5.31658 3.90249 4.68342 4.29302 4.29289Z" fill="#111827"/>
+          </svg>
+          <p className='pb-2'>
             O God, you granted your servant Montse the grace of serene and cheerful dedication to your will with admirable simplicity in everyday life. Grant that I may lovingly offer to you all my daily activity and convert it into Christian service for others. Deign to glorify your servant Montse and through her intercession grant me the favor I ask of you… (here make your request). Amen.
           </p>
-          <p>
+          <p className='pb-2'>
             Our Father, Hail Mary, Glory be to the Father
+          </p>
+          <p className="text-red font-frank">
+            Done with the prayer? click the X button to add your prayer.
           </p>
         </div>
         <div className='flex flex-col md:flex-row-reverse md:h-[100vh] md:border-b-2 md:border-r-2 border-black'>
